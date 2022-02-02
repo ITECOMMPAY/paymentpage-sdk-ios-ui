4c6dd4e6d4edc407857df0ef33fcc261c5a790f0
TrxHost <license-trxhost@hotmail.com>
2022-02-02 15:52:46 +0300

    IOS SDK

---
+++ IntegrationExampleObjc/IntegrationExampleObjc/Utils/Utils.swift
@@ -0,0 +1,16 @@
+//
+//  Utils.swift
+//  TestClient
+//
+//  Created by TrxHosts on 8/10/18.
+//  Copyright © 2018 TrxHosts. All rights reserved.
+//
+
+import UIKit
+
+class Utils: NSObject {
+    @objc(signature:secret:)
+    class func signature(paramsToSign: String, secret: String) -> String {
+        return paramsToSign.sha512(secret: secret)!.base64EncodedString();
+    }
+}
